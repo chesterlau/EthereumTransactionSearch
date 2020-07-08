2cb1531c8999d572aee501e535fd19b261b861d8
Chester Lau <chesterl@openpay.com.au>
2020-07-08 12:25:29 +1000

    Added skeleton code

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EthereumTransactionSearch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EthereumTransactionSearch.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : Controller
+    {
+        [HttpPost, Route("/api/search/Transactions")]
+        [ProducesResponseType(typeof(TransactionSearchResponse), 200)]
+        [ProducesResponseType(typeof(ApiResult), 400)]
+        public async Task<IActionResult> SearchTransactions()
+        {
+            return Ok();
+        }
+    }
+}
