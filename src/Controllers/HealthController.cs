b6706dd8fe77662592c59d9a5419a32dab55f806
Chester Lau <chesterl@openpay.com.au>
2020-07-11 12:16:14 +1000

    Added a health controller to allow health checks to be done

---
+++ src/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EthereumTransactionSearch.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
